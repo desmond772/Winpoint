c86df10bbd2c2d1b37176f690fd529a4119c085b
desmond772 <ngwad896@gmail.com>
2025-09-02 15:38:49 +0100

    Create config.py

---
+++ backend/backend/config.py
@@ -0,0 +1,11 @@
+TELEGRAM_API_ID = 123456
+TELEGRAM_API_HASH = 'your_api_hash_here'
+TELEGRAM_CHANNELS = ['channel1', 'channel2', 'channel3', 'channel4', 'channel5']
+
+CHANNEL_SETTINGS = {
+    'channel1': {'martingale_levels': 3, 'max_level': 3, 'default_amount': 10},
+    'channel2': {'martingale_levels': 5, 'max_level': 5, 'default_amount': 15},
+    'channel3': {'martingale_levels': 2, 'max_level': 2, 'default_amount': 20},
+    'channel4': {'martingale_levels': 4, 'max_level': 4, 'default_amount': 12},
+    'channel5': {'martingale_levels': 3, 'max_level': 3, 'default_amount': 8},
+}
